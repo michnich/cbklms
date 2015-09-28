17a9e26bc9e691bfc938650a47b4517a72c16fc7
smobleyii <smobleyii@gmail.com>
2015-09-28 10:16:44 -0400

    basic dashboard functionality

---
+++ client/templates/management.js
@@ -0,0 +1,20 @@
+Template.management.helpers({
+  studentsCountAct: function() {
+    return Students.find({status: "Active"}).count();
+  },
+  studentsCountChildAct: function() {
+    return Students.find({status: "Active", program: "Kids Weekly"}).count();
+  },
+  studentsCountAdultAct: function() {
+    return Students.find({status: "Active", program: "Dev Training"}).count();
+  },
+  studentsCountAllApplicant: function() {
+    return Students.find({status: "Applicant"}).count();
+  },
+  studentsCountKidsApplicant: function() {
+    return Students.find({status: "Applicant", program: "Kids Weekly"}).count();
+  },
+  studentsCountAdultApplicant: function() {
+    return Students.find({status: "Applicant", program: "Dev Training"}).count();
+  }
+});
